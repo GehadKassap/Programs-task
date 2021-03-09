140132e48b30f1f6e9bd8b55c2e34011cee9a737
gehadkassap <gohadkasaa@gmail.com>
2021-03-09 12:42:13 +0200

    create components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProgramsComponent } from './programs/programs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProgramsComponent
   ],
   imports: [
     BrowserModule,
